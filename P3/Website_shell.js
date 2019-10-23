1848548d5f3213eb8b4fe940cf364cfb376dfbea
Sander Høyland <sandereh@ntnu.no>
2019-10-23 15:18:59 +0200

    Første utkast til navigasjonsbar!

--- P3/Website_shell.js
+++ P3/Website_shell.js
@@ -15,8 +15,9 @@ document.body.style.margin = "0"
 
 //Styling for header-elementet
 header_element.style.display = "flex"
-header_element.style.gridArea = "1/1/2/4"
-header_element.style.backgroundColor = "gray"
+header_element.style.gridArea = "1/2/2/3"
+header_element.style.backgroundColor = "#a01405"
+header_element.style.justifyContent = "space-between"
 
 //Styling for center-elementet
 document.getElementsByClassName("center_element")[0].style.display = "flex"
@@ -29,16 +30,51 @@ footer_element.style.display = "flex"
 footer_element.style.gridArea = "3/1/4/4"
 footer_element.style.backgroundColor = "darkgray"
 
+//Styling for logobildet
+logo_image.style.maxHeight = "80px"
+
 //Legger header-element inn i body
 header_element.className = "header_element"
 document.body.appendChild(header_element)
 
 //Legger logo inn i header-element
 logo_image.id = "logo_image"
-logo_image.src = "Bilder/Logoer/Naborevyen_logo.png"
+logo_image.src = "../P3/Bilder/Logoer/Naborevyen_logo_cropped.png"
 logo_image.alt = " "
 header_element.appendChild(logo_image)
 
 //Legger footer-element inn i body
 footer_element.className = "footer_element"
 document.body.appendChild(footer_element)
+
+//Navbar-variabler
+let navbar = document.createElement("ul")
+let om_oss = document.createElement("li")
+let undergrupper = document.createElement("li")
+let arkiv = document.createElement("li")
+let påmelding = document.createElement("li")
+let billetter = document.createElement("li")
+
+//Navbar
+om_oss.appendChild(document.createElement("a"))
+undergrupper.appendChild(document.createElement("a"))
+arkiv.appendChild(document.createElement("a"))
+påmelding.appendChild(document.createElement("a"))
+billetter.appendChild(document.createElement("a"))
+om_oss.firstChild.href = "link.asp"
+om_oss.firstChild.innerText = "Om oss"
+undergrupper.firstChild.href = "link.asp"
+undergrupper.firstChild.innerText = "Undergrupper"
+arkiv.firstChild.href = "link.asp"
+arkiv.firstChild.innerText = "Arkiv"
+påmelding.firstChild.href = "link.asp"
+påmelding.firstChild.innerText = "Påmelding"
+billetter.firstChild.href = "link.asp"
+billetter.firstChild.innerText = "Billetter"
+navbar.appendChild(om_oss)
+navbar.appendChild(undergrupper)
+navbar.appendChild(arkiv)
+navbar.appendChild(påmelding)
+navbar.appendChild(billetter)
+header_element.appendChild(navbar)
+navbar.className = "navbar"
