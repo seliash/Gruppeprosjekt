35899109adc757f4f92be14a9e733bc82fcdbb83
Hans <hk-vie@hotmail.com>
2019-10-25 12:14:07 +0200

    Tullette knapper, & start på hvordan håndtere album og bilde itterasjon

--- P3/Arkiv.js
+++ P3/Arkiv.js
@@ -1,9 +1,32 @@
-
+var Albums=[];
 
 var body = document.getElementById("arkiv_body"); //arkiv boddyen
+
 img_6=document.getElementById("6");
 img_6.addEventListener('click',function(){clicked(img_6)});
+
+
 arkiv_bilder=document.getElementById("");
+
+
+class Album {
+	constructor(album_id,img_data) {
+		this.album_id=album_id
+		this.images=img_data
+		this.img_l=1
+		this.img_r=5
+	}
+}
+
+
+function load() {
+	var mydata = JSON.parse(bilder);
+	alert(mydata[0]);
+	Albums.push(Album,mydata)
+	console.log(Albums);
+
+}
+
 
 function clicked(img_element){ //bytter clasene hoverable med img_in_focus etter om bildet er trykket
     console.log(  "clicked nr " + img_element);
@@ -29,11 +52,34 @@ function clicked(img_element){ //bytter clasene hoverable med img_in_focus etter
 	}
 }
 
-function load() {
-	var mydata = JSON.parse(bilder);
-	//alert(mydata[0]);
 
+function iterate(dir){//ittererer album bilder i en retning TODO
 
+	pics=document.getElementById("Revy???")
+	butn=0;
+	console.log(pics);
+		console.log(pics.childNodes);
+	console.log(pics.childNodes.length);
 
-
+	for (var i = 0; i < pics.childNodes.length; i++) { //finner knappen, usikker på hvorfor...
+//console.log(	pics.childNodes[i]);
+		if (pics.childNodes[i].className=="knapp_"+dir) {
+			butn=pics.childNodes[i]
+		}
+	}
+	//super crude itterasjon igjennom:
+	if (dir=="l") {
+		load()
+		for (var i = 5; i < 16; i+=2) {//går til venstre
+			pics.childNodes[i].src=pics.childNodes[i+2].src;
+		}
+	}
+	else if (dir="r") {
+			for (var i = 15; i >5; i-=2) {//går til høyre
+				pics.childNodes[i].src=pics.childNodes[i-2].src;
+			}
+	}
 }
+
+function iterate_l(){iterate("l")}
+function iterate_r() { iterate("r")}
