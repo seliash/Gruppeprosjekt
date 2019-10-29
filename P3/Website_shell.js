6889707dd3687ab3ac759e9c3cafc51277d98c63
Sander Høyland <sandereh@ntnu.no>
2019-10-29 15:50:29 +0100

    Implementert resten av funksjonene til navigasjonsbaren. Dropdown fungerer og linker til undersidene :)

--- P3/Website_shell.js
+++ P3/Website_shell.js
@@ -48,10 +48,13 @@ document.body.appendChild(left_header_element)
 document.body.appendChild(right_header_element)
 
 //Legger logo inn i header-element
+logo_wrapper = document.createElement("a")
+logo_wrapper.appendChild(logo_image)
+logo_wrapper.href = "Hovedside.html"
 logo_image.id = "logo_image"
 logo_image.src = "../P3/Bilder/Logoer/Naborevyen_logo_cropped.png"
 logo_image.alt = " "
-header_element.appendChild(logo_image)
+header_element.appendChild(logo_wrapper)
 
 //Legger footer-element inn i body
 footer_element.className = "footer_element"
@@ -64,22 +67,70 @@ let undergrupper = document.createElement("li")
 let arkiv = document.createElement("li")
 let påmelding = document.createElement("li")
 let billetter = document.createElement("li")
+let undergrupper_dropdown = document.createElement("div")
+undergrupper.className = "dropdown_btn"
+undergrupper.id = "undergrupperID"
+undergrupper_dropdown.id = "undergrupper_dropdown"
+
+//undergrupper
+let styret = document.createElement("a")
+styret.innerText = "Styret"
+styret.href = "Undergrupper/Styret.html"
+undergrupper_dropdown.appendChild(styret)
+let bygg = document.createElement("a")
+bygg.innerText = "Bygg"
+bygg.href = "Undergrupper/Bygg.html"
+undergrupper_dropdown.appendChild(bygg)
+let dans = document.createElement("a")
+dans.innerText = "Dans"
+dans.href = "Undergrupper/Dans.html"
+undergrupper_dropdown.appendChild(dans)
+let kostymeOgRek = document.createElement("a")
+kostymeOgRek.innerText = "Kostyme og rekvisitta"
+kostymeOgRek.href = "Undergrupper/Kostyme_og_rekvisitter.html"
+undergrupper_dropdown.appendChild(kostymeOgRek)
+let musikk = document.createElement("a")
+musikk.innerText = "Musikk"
+musikk.href = "Undergrupper/Musikk.html"
+undergrupper_dropdown.appendChild(musikk)
+let økonomi = document.createElement("a")
+økonomi.innerText = "Økonomi"
+økonomi.href = "Undergrupper/Økonomi.html"
+undergrupper_dropdown.appendChild(økonomi)
+let prDesign = document.createElement("a")
+prDesign.innerText = "PR og design"
+prDesign.href = "Undergrupper/PR_og_design.html"
+undergrupper_dropdown.appendChild(prDesign)
+let skribent = document.createElement("a")
+skribent.innerText = "Skribent"
+skribent.href = "Undergrupper/Skribent.html"
+undergrupper_dropdown.appendChild(skribent)
+let teknikk = document.createElement("a")
+teknikk.innerText = "Teknikk"
+teknikk.href = "Undergrupper/Teknikk.html"
+undergrupper_dropdown.appendChild(teknikk)
+let velferd = document.createElement("a")
+velferd.innerText = "Velferd"
+velferd.href = "Undergrupper/Velferd.html"
+undergrupper_dropdown.appendChild(velferd)
 
 //Navbar
+//https://www.w3schools.com/css/tryit.asp?filename=trycss_dropdown_navbar
 om_oss.appendChild(document.createElement("a"))
 undergrupper.appendChild(document.createElement("a"))
 arkiv.appendChild(document.createElement("a"))
 påmelding.appendChild(document.createElement("a"))
 billetter.appendChild(document.createElement("a"))
-om_oss.firstChild.href = "link.asp"
+om_oss.firstChild.href = "Om_oss.html"
 om_oss.firstChild.innerText = "Om oss"
-undergrupper.firstChild.href = "link.asp"
+undergrupper.firstChild.href = "link.asp" //--Denne må endres!
 undergrupper.firstChild.innerText = "Undergrupper"
-arkiv.firstChild.href = "link.asp"
+undergrupper.appendChild(undergrupper_dropdown)
+arkiv.firstChild.href = "Arkiv.html"
 arkiv.firstChild.innerText = "Arkiv"
-påmelding.firstChild.href = "link.asp"
+påmelding.firstChild.href = "link.asp" //--Denne må endres!
 påmelding.firstChild.innerText = "Påmelding"
-billetter.firstChild.href = "link.asp"
+billetter.firstChild.href = "Billettsalg.html"
 billetter.firstChild.innerText = "Billetter"
 navbar.appendChild(om_oss)
 navbar.appendChild(undergrupper)
