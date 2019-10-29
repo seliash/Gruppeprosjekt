6e141ce51654bc7f89d41ba463c288870366088e
Hans <hk-vie@hotmail.com>
2019-10-29 20:11:08 +0100

    Har naive knapper til bildene

--- P3/Arkiv.js
+++ P3/Arkiv.js
@@ -24,6 +24,7 @@ function load() {
 		construct_album(album);
 	}*/
 }
+
 class Album {
 	constructor(album_id,img_data) {
 
@@ -35,11 +36,23 @@ class Album {
 	}
 }
 
+function getAlbum(album_id){
+	for (var i = 0; i < Albums.length; i++) {
+		if (Albums[i].album_id==album_id) {
+			return Albums[i]
+		}
+	}
+}
+function addDriveSrc(driveID) {
+ return "https://drive.google.com/uc?id="+ driveID
+}
+
 function construct_album(album){//skal injektere hele albummet i HTML
 //oppretter div - Arkiv_Bilder
 	album.album_node=document.createElement("div");
-	album.id=album.album_id;
+	album.album_node.id=album.album_id;
 	album.album_node.className="Arkiv_Bilder";
+
 
 //Oppretter h2-elementet
 	h2=document.createElement("h2")
@@ -50,84 +63,79 @@ function construct_album(album){//skal injektere hele albummet i HTML
 //Oppretter venstre button
 	lft_butn=document.createElement("button");
 	album.album_node.appendChild(lft_butn);
-	lft_butn.outerHTML= "<button class="+'"'+"knapp_l"+'"'+" onclick="+'"'+"iterate_l()"+'"'+"> Left </button>"
-// se høyre button for hva som skjer ovenfor.
+	lft_butn.className="knapp_l";
+	lft_butn.appendChild(document.createTextNode("left"))
+
+//	lft_butn.outerHTML= "<button class="+'"'+"knapp_l"+'"'+" onclick="+'"'+"iterate_l()"+'"'+"> Left </button>"
 
 // legger inn de første 5 bildene
 //  HTML- formen på bildet
 //  <img src="https://drive.google.com/uc?id=ID_bilde" class="hoverable" alt="Arkiv" id="ID_bilde">
-for (var i = 0; i < 5
-
-	; i++) {
-	im=document.createElement("img");
-	im.src= "https://drive.google.com/uc?id="+ album.images[i] ;
-	im.className= "hoverable";
-	im.alt="arkiv bilde";
-	im.id=album.images[i];
-	console.log("giv event listn:"+album.images[i]);
-
-
-	album.album_node.appendChild(im);
-	// legger til EventListener e er musepekeren, e.target er det den trykker på, altså bildet.
-im.addEventListener('click',function(e){ clicked(e.target.id)});
-
-}
-console.log(i);
-for (var i = 0; i < 10; i++) {
-	console.log(album.images[i]);
-}
-console.log(i);
-	/*img_6=document.getElementById("6");
-img_6.addEventListener('click',function(){clicked(img_6)});
-
-// Oppretter høyre button
-rgt_butn=document.createElement("button");
-album.album_node.appendChild(rgt_butn);
-rgt_butn.outerHTML= "<button class="+'"'+"knapp_r"+'"'+" onclick="+'"'+"iterate_r()"+'"'+"> Right </button>"
-/* Bruker outer HTML, men kan og bruke denne jsen: Selv om det å få en "onklick er kjipt slik"
-rgt_butn.className="knapp_r";
-rgt_butn.onclick="iterate_r()";
-rgt_butn.appendChild(document.createTextNode("right"));//legger til teksten left til knappen
-*/
-
-
-//legger inn albumet i arkivet:
-body.appendChild(album.album_node);
-
-}
-
-
-function iterate(dir){//ittererer album bilder i en retning TODO
-
-	pics=document.getElementById("Revy???")
-	butn=0;
-	console.log(pics);
-		console.log(pics.childNodes);
-	console.log(pics.childNodes.length);
-
-	for (var i = 0; i < pics.childNodes.length; i++) { //finner knappen, usikker på hvorfor...
-//console.log(	pics.childNodes[i]);
-		if (pics.childNodes[i].className=="knapp_"+dir) {
-			butn=pics.childNodes[i]
-		}
+	for (var i = 0; i < 5; i++) {
+		im=document.createElement("img");
+		im.src=addDriveSrc(album.images[i])
+		im.className= "hoverable";
+		im.alt="arkiv bilde";
+		im.id=album.images[i];
+		//console.log("giv event listn:"+album.images[i]);
+		album.album_node.appendChild(im);
+		// legger til EventListener e er musepekeren, e.target er det den trykker på, altså bildet.
+		im.addEventListener('click',function(e){ clicked(e.target.id)});
 	}
-	//super crude itterasjon igjennom:
-	if (dir=="l") {
+	// Oppretter høyre button
+	rgt_butn=document.createElement("button");
+	album.album_node.appendChild(rgt_butn);
+	rgt_butn.className="knapp_r";
+	rgt_butn.appendChild(document.createTextNode("right"));//legger til teksten left til knappen
 
-		for (var i = 5; i < 16; i+=2) {//går til venstre
-			pics.childNodes[i].src=pics.childNodes[i+2].src;
-		}
+
+	//legger til EventListenere for de to knappene.
+	lft_butn.addEventListener('click',function(e){iterate(e.target,'l')})
+	rgt_butn.addEventListener('click',function(e){iterate(e.target,'r')})
+
+	//legger inn albumet i arkivet:
+	body.appendChild(album.album_node);
+
+}
+
+
+function iterate(butn_node, dir){//ittererer album bilder i en retning TODO
+	console.log(butn_node, dir);
+	pics=butn_node.parentNode;	//album noden
+	album_data=getAlbum(pics.id); // data beholderen for albumet.
+	console.log(album);
+
+
+	//super crude "itterasjon" igjennom bildene, sjekker ikke om man er på kanten av bildene.
+	if (dir=="l") {
+		console.log("l");
+		pics.insertBefore(	pics.childNodes[1+5],pics.childNodes[1+1])//setter bilde nr 5 før bilde nr 1
+
+	//setter så bildet til venstre til å være det nye bildet.
+		newId=album_data.images[album_data.img_l-1];
+		pics.childNodes[1+1].src=addDriveSrc(newId);
+		pics.childNodes[1+1].id=newId;
+
+		//oppdaterer hvor bildepekerne står
+			album_data.img_r-=1;
+			album_data.img_l-=1;
 	}
 	else if (dir="r") {
-			for (var i = 15; i >5; i-=2) {//går til høyre
-				pics.childNodes[i].src=pics.childNodes[i-2].src;
-			}
+		console.log("r");
+		pics.insertBefore( pics.childNodes[1+1], pics.childNodes[7])
+//setter så bildet til høyre til å være det nye bildet:
+		newId=album_data.images[album_data.img_r+1];
+		pics.childNodes[1+5].src=addDriveSrc(newId);
+		pics.childNodes[1+5].id=newId;
+//oppdaterer hvor bildepekerne står
+	album_data.img_r+=1;
+	album_data.img_l+=1;
 	}
 }
-
+/*
 function iterate_l(){iterate("l")}
 function iterate_r() { iterate("r")}
-
+*/
 
 function clicked(img_id){ //bytter clasene hoverable med img_in_focus etter om bildet er trykket
 		img_element=document.getElementById(img_id);
@@ -141,6 +149,8 @@ function clicked(img_id){ //bytter clasene hoverable med img_in_focus etter om b
 	    //console.log(høyde);
 	    console.log("arkivbody,høyde "+body.clientHeight)
 	    img_element.classList.add("img_in_focus");
+			//// TODO: fikse img in focus for tilfeldige store bilder.
+			// TODO: Fikse slik at høyre knappen kommer under bildet i fokus.
 	    høyde=img_element.offsetHeight;
 	    arkiv_høyde=body.clientHeight;
 	    console.log(høyde +"  "+ arkiv_høyde);
